73b70bc6410fb45f2894d746708aa69022273980
DoraFi <darja.shved@gmail.com>
2025-09-25 22:19:05 +0300

    Бранч 1 gпереименовала файлики для мерджа

--- app/src/main/java/com/example/lab2git/MainActivityDashaShved.kt
+++ app/src/main/java/com/example/lab2git/MainActivityDashaShved.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.`activity_main_dashashved.xml`)
 
         resultText = findViewById(R.id.resultText)
         datePicker = findViewById(R.id.datePicker)
